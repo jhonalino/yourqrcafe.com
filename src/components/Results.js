524a4c09b22f638c1188c070ee62fc42daae8592
jhon alino <jhonfrancisalino@gmail.com>
2018-04-19 03:19:49 -0700

    redesigned encoded data, and fixed download button overflow

--- src/components/Results.js
+++ src/components/Results.js
@@ -20,50 +20,51 @@ export default class Results extends Component {
     const { data } = this.props;
     let svgUri = qrHelper.encodeDataToSvgUri(data);
     return (
-      <div className="box container-results">
-        <img src={imageHelper.generateSvgUrl(svgUri)} alt="" />
-        <div className="btns">
-          <div className="field has-addons has-addons-centered">
-            <p className="control is-expanded">
-              <button
-                id="dl-btn"
-                className="button is-info"
-                onClick={() => {
-                  imageHelper.downloadHandler(
-                    this.state.selectedFileType,
-                    svgUri
-                  );
-                }}
-              >
-                DOWNLOAD
-              </button>
-            </p>
-            <p className="control">
-              <span className="select is-info">
-                <select
-                  onChange={this.handleChange}
-                  value={this.state.selectedFileType}
+      <div>
+        <div className="box container-results">
+          <img src={imageHelper.generateSvgUrl(svgUri)} alt="" />
+          <div className="btns">
+            <div className="field has-addons has-addons-centered">
+              <p className="control is-expanded">
+                <button
+                  id="dl-btn"
+                  className="button is-info"
+                  onClick={() => {
+                    imageHelper.downloadHandler(
+                      this.state.selectedFileType,
+                      svgUri
+                    );
+                  }}
                 >
-                  <option value="png">.PNG</option>
-                  <option value="jpeg">.JPEG</option>
-                  <option value="svg">.SVG</option>
-                </select>
-              </span>
-            </p>
+                  DOWNLOAD
+                </button>
+              </p>
+              <p className="control">
+                <span className="select is-info">
+                  <select
+                    onChange={this.handleChange}
+                    value={this.state.selectedFileType}
+                  >
+                    <option value="png">.PNG</option>
+                    <option value="jpeg">.JPEG</option>
+                    <option value="svg">.SVG</option>
+                  </select>
+                </span>
+              </p>
+            </div>
           </div>
         </div>
-        <div className="encoded-data">
-          <p className="has-text-centered is-uppercase has-text-weight-bold">
-            Encoded Data: {data.type}
-          </p>
+        
+        <div className="box encoded-data">
+          <h3 className="title is-4 has-text-centered is-uppercase has-text-weight-bold">
+            Encoded Data
+          </h3>
           {Object.entries(data).map(([prop, value]) => (
             <div key={prop}>
               {prop !== "type" && (
                 <div key={prop}>
-                  <p>
-                    <span id="label">{prop}: </span>
-                    <i id="value">{value}</i>
-                  </p>
+                  <p class="title is-5">{prop}:</p>
+                  <p class="subtitle is-6">{value}</p>
                 </div>
               )}
             </div>
